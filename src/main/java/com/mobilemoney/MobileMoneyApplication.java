320686845f099120021fae0169c65022c23e4073
Andy Faniry <ramarosonandyfaniry@gmail.com>
2021-03-18 18:42:07 +0300

    spring

--- src/main/java/com/mobilemoney/MobileMoneyApplication.java
+++ src/main/java/com/mobilemoney/MobileMoneyApplication.java
@@ -1,13 +1,13 @@
-package com.test;
+package com.mobilemoney;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestApplication {
+public class MobileMoneyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
+		SpringApplication.run(MobileMoneyApplication.class, args);
 	}
 
 }
--- src/test/java/com/mobilemoney/MobileMoneyApplicationTests.java
+++ src/test/java/com/mobilemoney/MobileMoneyApplicationTests.java
@@ -1,10 +1,10 @@
-package com.test;
+package com.mobilemoney;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TestApplicationTests {
+class MobileMoneyApplicationTests {
 
 	@Test
 	void contextLoads() {
