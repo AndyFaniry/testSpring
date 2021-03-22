a7cfc6796c1dad19d81065bc972fa15ccbabbc64
Andy Faniry <ramarosonandyfaniry@gmail.com>
2021-03-22 21:02:14 +0300

    Update Operateur.java

--- src/main/java/com/mobilemoney/model/Operateur.java
+++ src/main/java/com/mobilemoney/model/Operateur.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import com.mobilemoney.bdb.ConnectionPstg;
+import com.mobilemoney.fonction.Fonction;
 
 public class Operateur {
 	 int idOperateur;
